2117a4ea3873f833d3153c0222f596ff86734f1c
EtkinD <etkin.dogan@hotmail.com>
2025-02-25 14:18:29 +0300

    Add docstring to Sahne class and restore string representation method

--- picengine/src/game/scene.py
+++ picengine/src/game/scene.py
@@ -1,11 +1,13 @@
 from ..objects import GameObject
 
-# Scene for the game
+"""
+    Sahne sınıfı, oyunun içindeki objeleri tutar ve bunları günceller ve çizer.
+"""
 class Sahne():
     def __init__(self, isim: str = 'normal sahne', oyun_objeleri: GameObject = []) -> None:
         self.__isim = isim
         self.__game_objects: list[GameObject] = oyun_objeleri
-    
+
     def guncelle(self, dt: float) -> None:
         for obje in self.__game_objects:
             obje.guncelle(dt)
@@ -16,21 +18,22 @@ class Sahne():
 
     def ekle(self, nesne: GameObject) -> None:
         self.__game_objects.append(nesne)
-    
-    def __str__(self):
-        return self.__isim
 
     @property
     def objeler(self) -> list[GameObject]:
         return self.__game_objects
-    
+
     @property
     def isim(self) -> str:
         return self.__isim
-    
+
     @isim.setter
     def isim(self, value: str) -> None:
         self.__isim = value
 
     def __iter__(self):
         return iter(self.__game_objects)
+
+    def __str__(self):
+        return self.__isim
+
